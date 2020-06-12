da559b115bf254b996a43880d6757b4c1e52ed66
Tom Smith <tom@t-smith.co.uk>
2020-06-12 20:32:21 +0100

    Adding a simple webserver first

--- cmd/streamwidget/main.go
+++ cmd/streamwidget/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("Hello, Cath, how are you?")
+	fmt.Println("Hello, Cath, how are you?")
+	http.HandleFunc("/", HelloServer)
+	log.Fatal(
+		http.ListenAndServe(":8080", nil),
+	)
+}
+
+// HelloServer is just a test function
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusNotAcceptable)
+	}
+	log.Println(r.URL.Path[1:])
 }
