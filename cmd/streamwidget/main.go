94db8cefa33550258ece520fb7f1859f9d5fe284
Tom Smith <tom@t-smith.co.uk>
2020-06-12 21:04:26 +0100

    Simple folder reader example

--- cmd/streamwidget/main.go
+++ cmd/streamwidget/main.go
@@ -2,16 +2,45 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fmt.Println("Hello, Cath, how are you?")
-	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/", FolderHandler)
 	log.Fatal(
 		http.ListenAndServe(":8080", nil),
 	)
+}
+
+// FolderHandler handles folder paths
+func FolderHandler(w http.ResponseWriter, r *http.Request) {
+	folder := r.URL.Path[1:]
+	file := fmt.Sprintf("./examples/%s/index.html", folder) // @todo: just testing code
+
+	f, err := os.Open(file)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+
+	_, err = fmt.Fprint(w, string(data))
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
 }
 
 // HelloServer is just a test function
